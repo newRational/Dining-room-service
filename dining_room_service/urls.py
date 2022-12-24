c2b6a8461c4848acf4060571bcf9d19e986977db
Egor Perevoshchikov <egorka7999@mail.ru>
2022-12-25 01:59:17 +0300

    Добавлены комментарии к dining_room_service/urls.py и dining_room_service/views.py

--- dining_room_service/urls.py
+++ dining_room_service/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from .views import show_funcs
 
+# Из словарика show_funcs достаются нужные
+# функции-контроллеры
 urlpatterns = [
 	path('users', show_funcs['user']),
 	path('products', show_funcs['product']),
--- dining_room_service/views.py
+++ dining_room_service/views.py
@@ -3,20 +3,29 @@ from django.template import loader
 
 from .models import *
 
+# Функция, которая создает функции-контроллеры
 def create_show_func(aClass, order_field='pk', show_field='pk'):
+
+	# Создаваемая функция-контроллер
 	def show_func(request):
+
+		# Путь к HTML шаблону страницы (templates/show.html, templates добавляет автоматически)
 		template = loader.get_template('show.html')
-		elements = aClass.objects.order_by(order_field)
+
+		# Чем будет заполнен шаблон
 		context = {
 			'title': aClass._meta.verbose_name_plural,
 			'header': aClass._meta.verbose_name_plural,
 			'show_field': show_field,
-			'elements': elements,
+			'elements': aClass.objects.order_by(order_field),
 		}
 		return HttpResponse(template.render(context, request))
 
 	return show_func
 
+
+# Словарик с функциями контроллерами. 
+# Этот словарик импортируется в dining_room_service/urls.py
 show_funcs = {
 	'user': create_show_func(User, 'username', 'username'),
 	'product': create_show_func(Product, 'name', 'name'),
