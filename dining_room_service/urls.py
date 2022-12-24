83cb8c0e3a0cbca9ccaebe80ccdb75c726cf1acb
Egor Perevoshchikov <egorka7999@mail.ru>
2022-12-25 01:51:20 +0300

    В urlpatterns изменено 'dishs' на 'dishes'

--- dining_room_service/urls.py
+++ dining_room_service/urls.py
@@ -5,7 +5,7 @@ from .views import show_funcs
 urlpatterns = [
 	path('users', show_funcs['user']),
 	path('products', show_funcs['product']),
-	path('dishs', show_funcs['dish']),
+	path('dishes', show_funcs['dish']),
 	path('diningrooms', show_funcs['diningroom']),
 	path('comments', show_funcs['comment']),
 ]
